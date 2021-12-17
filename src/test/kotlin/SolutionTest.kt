3babf204decb8fbb0b4ee133a4da650f1912c7fb
Ken Wu <cia.exe@msn.com>
2021-12-18 01:44:51 +0800

    Apply TimingExtension to log the execution time of each Junit5 test case.

--- src/test/kotlin/SolutionTest.kt
+++ src/test/kotlin/SolutionTest.kt
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.extension.ExtendWith
 
+@ExtendWith(TimingExtension::class)
 internal class SolutionTest {
 
     @Test
