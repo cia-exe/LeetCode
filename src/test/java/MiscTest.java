3cdbb5c9d783aebfb840fffde8e675f13753a85d
Ken Wu(伍偉華) <Ken_Wu@HTC.COM>
2022-03-29 10:42:20 +0800

    add java toList test

--- src/test/java/MiscTest.java
+++ src/test/java/MiscTest.java
@@ -64,8 +64,8 @@ public class MiscTest {
         out.println(l3);
         l3.sort(Comparator.reverseOrder());
         out.println(l3);
-        var arrObj =l3.toArray();
-        var arrInt =l3.toArray(new Integer[0]);
+        var arrObj = l3.toArray();
+        var arrInt = l3.toArray(new Integer[0]);
         out.println(Arrays.toString(arrObj));
         out.println(Arrays.toString(arrInt));
 
@@ -107,8 +107,8 @@ public class MiscTest {
         // Don't use Stack anymore, it's very old java and relies on Vector.
         // Instead of use Deque, implemented by e.g. ArrayDeque or LinkedList.
 
-        // ArrayDeque 和 Stack 類都可以，但 ArrayDeque 效率高於 Stack，
-        // 功能也比 Stack 多，Java 已不推薦使用 Stack，而推薦 ArrayDeque，次選 LinkedList
+        // ArrayDeque performance is better than Stack and has more functions.
+        // 1st choose is ArrayDeque, 2nd LinkedList.
 
         out.println("stack------------");
         var s = new Stack<Integer>();
@@ -141,7 +141,7 @@ public class MiscTest {
         q.peekLast();
 
         out.println(" array queue-----------");
-        var d= new ArrayDeque<Integer>();
+        var d = new ArrayDeque<Integer>();
 
 
         out.println("heap-----------");
@@ -151,8 +151,8 @@ public class MiscTest {
         int o;
         h.add(1); // exception if failed
         h.offer(2); // false if failed
-        o=h.remove(); // exception if empty
-        o=h.poll(); // null if empty
+        o = h.remove(); // exception if empty
+        o = h.poll(); // null if empty
 
         h.peek();
         h.contains(2);
@@ -160,12 +160,63 @@ public class MiscTest {
         // HashSet non-ordered
         // TreeSet ordered
 
-        var tSet= new TreeSet<Integer>();
-        var hSet=new HashSet<Integer>();
-        var tMap=new TreeMap<Integer,Integer>();
-        var hMap=new HashMap<Integer,Integer>();
-        var map=Map.of("Justin", 95, "Monica", 100);
-        var set = Set.of(1,2,3);
+        var tSet = new TreeSet<Integer>();
+        var hSet = new HashSet<Integer>();
+        var tMap = new TreeMap<Integer, Integer>();
+        var hMap = new HashMap<Integer, Integer>();
+        var map = Map.of("Justin", 95, "Monica", 100);
+        var set = Set.of(1, 2, 3);
 
     }
+
+    @Test
+    public void intToList() {
+        var a1 = new int[]{1, 2, 3};
+        int[] a2 = {1, 2, 3}; // the same as above. just another syntax.
+
+        // List<int[]> has only one element.
+        var l1 = List.of(a1);
+        var l2 = Arrays.asList(a2);     // suggest replacing with List.of()
+        out.println(l1.size() + l2.size());       // only 2!!!, not 6!!!
+
+        // l1.add(new int[]{4,5,6}); //java.lang.UnsupportedOperationException
+        // l2.add(new int[]{4,5,6}); //java.lang.UnsupportedOperationException (Immutable)
+
+        a1[0] = 0;
+        a2[0] = 0;
+
+        out.println(Arrays.toString(a1));       // [0, 2, 3]     !!!share tha same array.
+        l1.forEach(System.out::println);        // [I@55cb6996
+        out.println(Arrays.toString(l1.get(0)));// [0, 2, 3]
+
+        out.println(Arrays.toString(a2));       // the same as above
+        l2.forEach(System.out::println);
+        out.println(Arrays.toString(l2.get(0)));
+
+    }
+
+    @Test
+    public void integerToList() {
+        var a1 = new Integer[]{1, 2, 3 /*, null*/};
+        Integer[] a2 = {1, 2, 3, null};
+
+        // List<Integer>
+        var l1 = List.of(a1); // NullPointerException if a1 has null!!!
+        var l2 = Arrays.asList(a2);
+        out.println(l1.size() + l2.size());   // 6
+
+
+        //l1.add(4); // UnsupportedOperationException
+        //l2.add(4); // UnsupportedOperationException (Immutable)
+
+        a1[0] = 9;
+        a2[0] = 9;
+
+        out.println(Arrays.toString(a1)); // [9, 2, 3]
+        l1.forEach(System.out::println);  // [1, 2, 3]
+
+        out.println(Arrays.toString(a2)); // [9, 2, 3, null]
+        l2.forEach(System.out::println);  // [9, 2, 3, null] !!! share tha same array.
+    }
+
 }
